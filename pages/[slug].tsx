0cf12d5fec8912673c7b4a18e4d561882d14563f
chuppyy <takhanhthient3@GMAIL.COM>
2024-04-10 06:43:14 +0700

    k2

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -32,6 +32,10 @@ export default function App({ url, title, description, icon, image }: any) {
   return (
     <>
       <Head>       
+      <title>{title}</title>
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content={image} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={icon} />
       </Head>
