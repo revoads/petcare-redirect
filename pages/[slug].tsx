d07311d25ab609df1b6ca630da8d3d909ff309fd
revoads <119649285+revoads@users.noreply.github.com>
2026-06-11 15:32:04 +0700

    Update [slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -13,7 +13,7 @@ export async function getServerSideProps(context: any) {
         permanent: false,
         destination: domain + "" + context.params.slug,
       },
-  
+
     };
   }
   const data = await fetchMeta(domain + "" + context.params.slug);
